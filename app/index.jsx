cc1c46e062d4062be196765c02ce5cd993a20bb5
Mathis Kretz <mathis.kretz@gmail.com>
2019-02-04 16:34:44 +0100

    Remove leftover websocket stuff

--- app/index.jsx
+++ app/index.jsx
@@ -6,14 +6,10 @@ import injectTapEventPlugin from "react-tap-event-plugin";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 import HeaderContainer from "./header/headerContainer.jsx";
 import NotificationsContainer from "./notifications/notificationsContainer.jsx";
 import { routerMiddleware, syncHistoryWithStore } from "react-router-redux";
-import io from "socket.io-client";
 import reducer from "./reducer.jsx";
-
-const socket = io.connect("https://socketio-echo.scapp.io");
 
 injectTapEventPlugin();
 
@@ -21,10 +17,7 @@ const routerMiddlewareBrowserHistory = routerMiddleware(browserHistory);
 
 let store = createStore(
   reducer,
-  applyMiddleware(
-    routerMiddlewareBrowserHistory,
-    thunk.withExtraArgument(socket)
-  )
+  applyMiddleware(routerMiddlewareBrowserHistory)
 );
 
 const history = syncHistoryWithStore(browserHistory, store);
