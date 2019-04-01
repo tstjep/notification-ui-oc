bd10ef83fcf9c7e2a3f13a959f835668f35b07c8
Mathis Kretz <mathis.kretz@gmail.com>
2019-04-01 19:18:29 +0200

    Trigger a build (information)

--- app/header/header.jsx
+++ app/header/header.jsx
@@ -6,7 +6,7 @@ import NotificationsIcon from "material-ui/svg-icons/social/notifications";
 const Header = ({ children, notifications }) => (
   <div>
     <AppBar
-      title="Notifications"
+      title="Informations!!"
       iconElementRight={
         <Badge badgeContent={notifications.length} secondary={true}>
           <NotificationsIcon />
